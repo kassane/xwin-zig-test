f557d8e4d03641b8469134a93846677a2e483246
Matheus Catarino <matheus-catarino@hotmail.com>
2025-05-21 13:44:14 -0300

    updated to zig v0.14.0

--- src/hello-c.c
+++ src/hello-c.c
@@ -1,47 +1,46 @@
 #include <stdio.h>
+#ifdef _WIN32
 #include <windows.h>
 
-DWORD WINAPI sleepSort(LPVOID num)
-{
-    int sleepTime = *((int*)num);
-    Sleep(sleepTime);
-    printf("%d ", sleepTime);
-    return 0;
+DWORD WINAPI sleepSort(LPVOID num) {
+  int sleepTime = *((int *)num);
+  Sleep(sleepTime);
+  printf("%d ", sleepTime);
+  return 0;
 }
 
-void sleepSortArray(int arr[], int size)
-{
-    HANDLE threads[size];
-    DWORD threadIDs[size];
-    int i;
+void sleepSortArray(int arr[], int size) {
+  HANDLE threads[size];
+  DWORD threadIDs[size];
+  int i;
 
-    for (i = 0; i < size; i++)
-    {
-        threads[i] = CreateThread(NULL, 0, sleepSort, (LPVOID)&arr[i], 0, &threadIDs[i]);
-    }
+  for (i = 0; i < size; i++) {
+    threads[i] =
+        CreateThread(NULL, 0, sleepSort, (LPVOID)&arr[i], 0, &threadIDs[i]);
+  }
 
-    WaitForMultipleObjects(size, threads, TRUE, INFINITE);
+  WaitForMultipleObjects(size, threads, TRUE, INFINITE);
 
-    for (i = 0; i < size; i++)
-    {
-        CloseHandle(threads[i]);
-    }
+  for (i = 0; i < size; i++) {
+    CloseHandle(threads[i]);
+  }
 }
+#endif
 
-int main()
-{
-    printf("Sleep sorting\n");
-    
-    int arr[] = {9, 40, 10, 1, 6, 45, 23, 50};
-    int size = sizeof(arr) / sizeof(arr[0]);
+int main() {
+  printf("Sleep sorting\n");
 
-    for (int i = 0; i < size; i++)
-    {
-        printf("%d ", arr[i]);
-    }
-    printf("\nSorted numbers: ");
-    sleepSortArray(arr, size);
-    printf("\n");
+  int arr[] = {9, 40, 10, 1, 6, 45, 23, 50};
+  int size = sizeof(arr) / sizeof(arr[0]);
 
-    return 0;
+  for (int i = 0; i < size; i++) {
+    printf("%d ", arr[i]);
+  }
+#ifdef _WIN32
+  printf("\nSorted numbers: ");
+  sleepSortArray(arr, size);
+  printf("\n");
+#endif
+
+  return 0;
 }
